edcb16f6f0c2d965c7bd01266503f1f4ad696adf
Oleksandr0007 <yagodinetc@gmail.com>
2022-12-17 03:20:04 +0200

    start

---
+++ app/js/main.js
@@ -0,0 +1,17 @@
+$(function(){
+
+    $('.header__slider').slick({
+        infinite: true,
+        fade: true,
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt=""></img>',
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt=""></img>',
+        asNavFor: '.slider-dots',
+    });
+
+    $('.slider-dots').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        asNavFor: '.header__slider',
+    });
+
+});
