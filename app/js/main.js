f67b2586c80e5d74fe012a259657fdf681de9383
Oleksandr0007 <yagodinetc@gmail.com>
2022-12-20 16:39:00 +0200

    add

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,5 @@
 $(function(){
-
+// header main slider
     $('.header__slider').slick({
         infinite: true,
         fade: true,
@@ -7,13 +7,15 @@ $(function(){
         nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt=""></img>',
         asNavFor: '.slider-dotshead',
     });
-
+    // header dots slider
     $('.slider-dotshead').slick({
         slidesToShow: 4,
         slidesToScroll: 4,
         asNavFor: '.header__slider',
     });
-
+    // end header dots slider
+// end header main slider
+// block SURF main slider
     $('.serf-slider').slick({
         slidesToShow: 4, 
         slidesToScroll: 1,
@@ -21,7 +23,8 @@ $(function(){
         nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt=""></img>',
         asNavFor: '.slider-map'
     });
-
+// end block SURF main slider
+// block SURF slider dots on map
     $('.slider-map').slick({
         slidesToShow: 8,
         slidesToScroll: 1,
@@ -29,5 +32,5 @@ $(function(){
         asNavFor: '.serf-slider',
         focusOnSelect: true
     });
-
+// end block SURF slider dots on map
 });
