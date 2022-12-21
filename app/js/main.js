74759c4083e5ae720ef90ab88ce7ef6ab298b180
Oleksandr0007 <yagodinetc@gmail.com>
2022-12-22 00:44:40 +0200

    Add
    index.html file done

--- app/js/main.js
+++ app/js/main.js
@@ -33,8 +33,8 @@ $(function(){
         focusOnSelect: true
     });
 // end block SURF slider dots on map
-// block TRAVEL main slider
-    $('.holder__slider').slick({
+// block TRAVEL main slider and block SHOP main slider
+    $('.holder__slider, .shop__slider').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
         infinite: true,
@@ -42,9 +42,8 @@ $(function(){
         prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt=""></img>',
         nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt=""></img>',
     });
-// end block TRAVEL main slider
+// end block TRAVEL main slider and block SHOP main slider
 // calc sum og night and guests script
-
     $('.quantity-button').on('click', function(){
         let summ = $('.nights').val() * $('.summ').data('nights') + ($('.guests').val() - 1) * $('.summ').data('guests');
         $('.summ').html('$' + summ)
@@ -52,10 +51,13 @@ $(function(){
 
     let summ = $('.nights').val() * $('.summ').data('nights') + ($('.guests').val() - 1) * $('.summ').data('guests');
     $('.summ').html('$' + summ)
-
 // end calc sum og night and guests script
+// block SHOP animation clock on dots at board
+    $('.surfboard-box__circle').on('click', function () {
+        $(this).toggleClass('active')
+    });
+// end block SHOP animation clock on dots at board
 });
-
 
 // input type number script
 $('<div class="quantity-nav"><div class="quantity-button quantity-up"><img src="img/plus.svg" alt=""></div><div class="quantity-button quantity-down"><img src="img/minus.svg" alt=""></div></div>').insertAfter('.quantity input');
@@ -88,6 +90,5 @@ $('.quantity').each(function () {
         spinner.find("input").val(newVal);
         spinner.find("input").trigger("change");
     });
-
 });
 // end input type number script
